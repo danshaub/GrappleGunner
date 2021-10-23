4ca48f01738d332f47040392b6014a7201ace549
Dan Haub <43016331+danshaub@users.noreply.github.com>
2021-10-23 11:29:15 -0700

    Added basic jump button

--- Grapple Gunner/Assets/Scripts/PlayerManager.cs
+++ Grapple Gunner/Assets/Scripts/PlayerManager.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerManager : MonoBehaviour
 {
+    public InputActionReference jumpReference = null;
+
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        jumpReference.action.started += Jump;
     }
 
     // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        jumpReference.action.started -= Jump;
+    }
+
+    private void Jump(InputAction.CallbackContext context)
+    {
+        Debug.Log("hello jump");
+        transform.Translate(new Vector3(0,1,0));
     }
 }
