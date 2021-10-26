1520f1a43a2e61dbc151d5eb6020a67aa8f26619
Dan Haub <43016331+danshaub@users.noreply.github.com>
2021-10-25 22:07:02 -0700

    Update TeleportationManager.cs
    slight refactor and adds comments

--- Grapple Gunner/Assets/Scripts/TeleportationManager.cs
+++ Grapple Gunner/Assets/Scripts/TeleportationManager.cs
@@ -5,20 +5,25 @@ using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
 public class TeleportationManager : MonoBehaviour
 {
+    // Protects against falling through the floor when teleporting
     public float teleportationOffset = 0.01f;
+
+    // Input actions for teleport buttons
     [SerializeField] private InputActionReference teleportRightHand;
     [SerializeField] private InputActionReference teleportLeftHand;
 
+    // XR Interaction Components
     [SerializeField] private XRRayInteractor rayInteractorRight;
     [SerializeField] private XRRayInteractor rayInteractorLeft;
-
     [SerializeField] private TeleportationProvider teleportationProvider;
 
     private void Awake() 
     {    
+        // Enable interactions
         rayInteractorRight.enabled = false;
         rayInteractorLeft.enabled = false;
 
+        // Subscribe to necessary events
         teleportRightHand.action.started += OnTeleportActivateRight;
         teleportRightHand.action.canceled += OnTeleportCancelRight;
         teleportLeftHand.action.started += OnTeleportActivateLeft;
@@ -28,53 +33,57 @@ public class TeleportationManager : MonoBehaviour
 
     private void OnTeleportActivateRight(InputAction.CallbackContext context)
     {
+        // Enable line renderer
         rayInteractorRight.enabled = true;
+
+        // Disable left hand teleporting
         teleportLeftHand.action.started -= OnTeleportActivateLeft;
-        teleportLeftHand.action.canceled -= OnTeleportCancelLeft;
     }
     private void OnTeleportActivateLeft(InputAction.CallbackContext context)
     {
+        // Enable line rendered
         rayInteractorLeft.enabled = true;
+
+        // Disable right hand teleporting
         teleportRightHand.action.started -= OnTeleportActivateRight;
-        teleportRightHand.action.canceled -= OnTeleportCancelRight;
     }
 
     private void OnTeleportCancelRight(InputAction.CallbackContext context)
     {
-        
+        // Check if raycast hits something 
+        if(rayInteractorRight.TryGetCurrent3DRaycastHit(out RaycastHit hit)){
 
-        if(!rayInteractorRight.TryGetCurrent3DRaycastHit(out RaycastHit hit)){
-            rayInteractorRight.enabled = false;
-            return;
+            //Create new teleport request for raycast hit position
+            TeleportRequest request = new TeleportRequest()
+            {
+                destinationPosition = hit.point + (Vector3.up * teleportationOffset)
+            };
+
+            // Queue teleport request
+            teleportationProvider.QueueTeleportRequest(request);
         }
 
-        TeleportRequest request = new TeleportRequest(){
-            destinationPosition = hit.point + (Vector3.up * teleportationOffset)
-        };
-
-        teleportationProvider.QueueTeleportRequest(request);
+        // Disable line renderer and enable left hand teleporting
         rayInteractorRight.enabled = false;
-
         teleportLeftHand.action.started += OnTeleportActivateLeft;
-        teleportLeftHand.action.canceled += OnTeleportCancelLeft;
     }
 
     private void OnTeleportCancelLeft(InputAction.CallbackContext context)
     {
+        // Check if raycast hits something 
+        if(rayInteractorLeft.TryGetCurrent3DRaycastHit(out RaycastHit hit)){
+            //Create new teleport request for raycast hit position
+            TeleportRequest request = new TeleportRequest()
+            {
+                destinationPosition = hit.point + (Vector3.up * teleportationOffset)
+            };
 
-        if(!rayInteractorLeft.TryGetCurrent3DRaycastHit(out RaycastHit hit)){
-            rayInteractorLeft.enabled = false;
-            return;
+            // Queue teleport request
+            teleportationProvider.QueueTeleportRequest(request);
         }
 
-        TeleportRequest request = new TeleportRequest(){
-            destinationPosition = hit.point + (Vector3.up * teleportationOffset)
-        };
-
-        teleportationProvider.QueueTeleportRequest(request);
+        // Disable line renderer and enable right hand teleporting
         rayInteractorLeft.enabled = false;
-
         teleportRightHand.action.started += OnTeleportActivateRight;
-        teleportRightHand.action.canceled += OnTeleportCancelRight;
     }
 }
