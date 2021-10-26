ac607aeada6b2ad4f507db2b707a7bcb656c03b3
Dan Haub <43016331+danshaub@users.noreply.github.com>
2021-10-25 21:53:42 -0700

    Cleaned up teleportation system
    Changes teleportation button to clicking control stick and hides teleport arc when not in use

--- Grapple Gunner/Assets/Scripts/PlayerManager.cs
+++ Grapple Gunner/Assets/Scripts/PlayerManager.cs
@@ -5,18 +5,37 @@ using UnityEngine.InputSystem;
 
 public class PlayerManager : MonoBehaviour
 {
-    public float jumpAmount = 10;
-    public InputActionReference jumpReference = null;
+    // Controller Prperties
+    public float jumpAmount = 5;
+    public float moveSpeed = 5;
 
-    private Rigidbody rigidbody = null;
+    public bool continuousMoveEnabled = true;
+
+    private Vector3 movementVector = Vector3.zero;
+  
+
+    // Controller Input Actions
+    public InputActionReference jumpReference = null;
+    public InputActionReference moveReference = null;
+
+    private Rigidbody rb;
 
     // Start is called before the first frame update
     private void Awake()
     {
+        rb = GetComponent<Rigidbody>();
         // subscribe to events
         jumpReference.action.started += Jump;
+        moveReference.action.performed += ContinuousMove;
+        
+    }
 
-        rigidbody = GetComponent<Rigidbody>();
+    private void FixedUpdate() {
+        #region Move Player
+
+        rb.MovePosition(transform.position + Time.fixedDeltaTime * moveSpeed * transform.TransformDirection(movementVector));
+
+        #endregion
     }
 
     // Update is called once per frame
@@ -27,7 +46,16 @@ public class PlayerManager : MonoBehaviour
 
     private void Jump(InputAction.CallbackContext context)
     {
-        Debug.Log("Jump");
-        rigidbody.velocity = Vector3.up * jumpAmount;
+        GetComponent<Rigidbody>().velocity = Vector3.up * jumpAmount;
+    }
+
+    private void ContinuousMove(InputAction.CallbackContext context){
+        if(continuousMoveEnabled) {
+            Vector2 controlValue = context.ReadValue<Vector2>();
+            movementVector = new Vector3(controlValue.x, 0, controlValue.y);
+        }
+        else{
+            movementVector = Vector3.zero;
+        }
     }
 }
--- Grapple Gunner/Assets/Scripts/TeleportationManager.cs
+++ Grapple Gunner/Assets/Scripts/TeleportationManager.cs
@@ -5,15 +5,17 @@ using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
 public class TeleportationManager : MonoBehaviour
 {
+    public float teleportationOffset = 0.01f;
     [SerializeField] private InputActionReference teleportRightHand;
     [SerializeField] private InputActionReference teleportLeftHand;
 
     [SerializeField] private XRRayInteractor rayInteractorRight;
     [SerializeField] private XRRayInteractor rayInteractorLeft;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField] private TeleportationProvider teleportationProvider;
+
+    private void Awake() 
+    {    
         rayInteractorRight.enabled = false;
         rayInteractorLeft.enabled = false;
 
@@ -24,31 +26,55 @@ public class TeleportationManager : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //if()
-    }
-
     private void OnTeleportActivateRight(InputAction.CallbackContext context)
     {
         rayInteractorRight.enabled = true;
+        teleportLeftHand.action.started -= OnTeleportActivateLeft;
+        teleportLeftHand.action.canceled -= OnTeleportCancelLeft;
     }
     private void OnTeleportActivateLeft(InputAction.CallbackContext context)
     {
         rayInteractorLeft.enabled = true;
+        teleportRightHand.action.started -= OnTeleportActivateRight;
+        teleportRightHand.action.canceled -= OnTeleportCancelRight;
     }
 
     private void OnTeleportCancelRight(InputAction.CallbackContext context)
     {
+        
+
+        if(!rayInteractorRight.TryGetCurrent3DRaycastHit(out RaycastHit hit)){
+            rayInteractorRight.enabled = false;
+            return;
+        }
+
+        TeleportRequest request = new TeleportRequest(){
+            destinationPosition = hit.point + (Vector3.up * teleportationOffset)
+        };
+
+        teleportationProvider.QueueTeleportRequest(request);
         rayInteractorRight.enabled = false;
+
+        teleportLeftHand.action.started += OnTeleportActivateLeft;
+        teleportLeftHand.action.canceled += OnTeleportCancelLeft;
     }
 
     private void OnTeleportCancelLeft(InputAction.CallbackContext context)
     {
+
+        if(!rayInteractorLeft.TryGetCurrent3DRaycastHit(out RaycastHit hit)){
+            rayInteractorLeft.enabled = false;
+            return;
+        }
+
+        TeleportRequest request = new TeleportRequest(){
+            destinationPosition = hit.point + (Vector3.up * teleportationOffset)
+        };
+
+        teleportationProvider.QueueTeleportRequest(request);
         rayInteractorLeft.enabled = false;
+
+        teleportRightHand.action.started += OnTeleportActivateRight;
+        teleportRightHand.action.canceled += OnTeleportCancelRight;
     }
-
-
-}
 }
