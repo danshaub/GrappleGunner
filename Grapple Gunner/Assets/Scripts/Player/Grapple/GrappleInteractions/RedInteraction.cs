a5c0b6ec40d01de920ba9df1ebfa8dac5cc25ed8
Dan Haub <43016331+danshaub@users.noreply.github.com>
2022-03-05 09:09:39 -0800

    Added stopping and reversing with red

--- Grapple Gunner/Assets/Scripts/Player/Grapple/GrappleInteractions/RedInteraction.cs
+++ Grapple Gunner/Assets/Scripts/Player/Grapple/GrappleInteractions/RedInteraction.cs
@@ -9,6 +9,9 @@ public class RedInteraction : I_GrappleInteraction
     private Rigidbody playerRB;
     private Vector3 ropeDirection;
 
+    private float breakAmount;
+    private bool reverse;
+
     public void OnHit(Transform gunTip, Transform hookPoint){
         props = GrappleManager._instance.redProperties;
         playerRB = PlayerManager._instance.movementController.rigidbody;
@@ -16,11 +19,14 @@ public class RedInteraction : I_GrappleInteraction
         currentHookPoint = hookPoint;
 
         PlayerManager._instance.allowMovement = false;
-        PlayerManager._instance.movementController.useGravity = false;
+        PlayerManager._instance.useGravity = false;
+        PlayerManager._instance.useFriction = false;
+
     }
     public void OnRelease(){
         PlayerManager._instance.allowMovement = true;
-        PlayerManager._instance.movementController.useGravity = true;
+        PlayerManager._instance.useGravity = true;
+        PlayerManager._instance.useFriction = true;
 
         playerRB.velocity = Vector3.zero;
     }
@@ -30,15 +36,23 @@ public class RedInteraction : I_GrappleInteraction
 
         float multiplier = props.redVelocityCurve.Evaluate(distanceFromPoint);
         Vector3 targetVelocity = -ropeDirection * props.redGrappleSpeed * multiplier;
+        targetVelocity *= (1-breakAmount);
 
+        if(reverse){
+            targetVelocity *= -1;
+        }
+        
         float damper = multiplier >= 1 ? props.redVelocityDamper : 1;
         playerRB.velocity = Vector3.LerpUnclamped(playerRB.velocity, targetVelocity, damper);
+
+
+        reverse = false;
     }
     public void OnReelIn(float reelStrength){
-        Debug.Log("Red R_In");
+        breakAmount = reelStrength;
     }
     public void OnReelOut(){
-        Debug.Log("Red R_Out");
+        reverse = true;
     }
     public void OnSwing(Vector3 swingVelocity){
         return;
