ed11adba776585a758dc3a041cab735efc3f6164
Jackson Goldberg <jagoldberg@chapman.edu>
2022-04-01 11:49:46 -0700

    Update PressurePlate.cs

--- Grapple Gunner/Assets/_Scripts/Mechanics/PressurePlate.cs
+++ Grapple Gunner/Assets/_Scripts/Mechanics/PressurePlate.cs
@@ -9,6 +9,17 @@ public class PressurePlate : MonoBehaviour
     public UnityEvent onPressurePlatePress;
     public UnityEvent onPressurePlateRelease;
 
+    public Vector3 anchorPosition;
     private bool released = false;
 
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Color c = Color.green;
+        c.a = .25f;
+        Gizmos.color = c;
+        Gizmos.DrawCube(anchorPosition, Vector3.one * .05f);
+    }
+#endif
 }
