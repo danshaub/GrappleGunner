440a408631e0ae3ab83e78a41eaaed85b0e13c43
Dan Haub <43016331+danshaub@users.noreply.github.com>
2022-04-05 15:05:46 -0700

    sets up project to add sounds

--- Grapple Gunner/Assets/_Scripts/GameManagement/Sound.cs
+++ Grapple Gunner/Assets/_Scripts/GameManagement/Sound.cs
@@ -9,9 +9,5 @@ public class Sound {
     [Range(0f,1f)]
     public float volume;
     [Range(0.1f, 3f)]
-    public float pitch = 1f;
-    public bool loop = false;
-
-    [HideInInspector]
-    public AudioSource source;
+    public float pitch;
 }
