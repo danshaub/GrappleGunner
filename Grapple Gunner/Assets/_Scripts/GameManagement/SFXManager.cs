4d2b19f51d0945cd7e90943164f2326d7e44cb72
Dan Haub <43016331+danshaub@users.noreply.github.com>
2022-04-05 15:05:46 -0700

    sets up project to add sounds

--- Grapple Gunner/Assets/_Scripts/GameManagement/SFXManager.cs
+++ Grapple Gunner/Assets/_Scripts/GameManagement/SFXManager.cs
@@ -1,8 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.Audio;
 using UnityEngine;
 
 public class SFXManager : SingletonPersistent<SFXManager>
 {
-
+    public Sound[] sounds;
 }
